a20e3b1c8f6c1df91f97e154577fd302b0f5bd43
codexp <codexp@gmx.net>
2018-03-20 21:11:21 +0100

    translate creative mod

--- mods/creative/inventory.lua
+++ mods/creative/inventory.lua
@@ -1,5 +1,14 @@
 local player_inventory = {}
 
+-- Intllib
+local S
+if minetest.get_modpath("intllib") then
+	S = intllib.Getter()
+else
+	S = function(s) return s end
+end
+player_inventory.intllib = S
+
 function creative.init_creative_inventory(player)
 	local player_name = player:get_player_name()
 	player_inventory[player_name] = {
@@ -98,8 +107,10 @@ function creative.register_tab(name, title, items)
 					button[7.25,3.2;0.8,0.9;creative_next;>]
 					button[2.1,3.4;0.8,0.5;creative_search;?]
 					button[2.75,3.4;0.8,0.5;creative_clear;X]
-					tooltip[creative_search;Search]
-					tooltip[creative_clear;Reset]
+				]] ..
+					"tooltip[creative_search;" .. S("Search") .. "]" ..
+					"tooltip[creative_clear;" .. S("Reset") .. "]" ..
+				[[
 					listring[current_player;main]
 					field_close_on_enter[creative_filter;false]
 				]] ..
@@ -162,10 +173,10 @@ minetest.register_on_joinplayer(function(player)
 	creative.update_creative_inventory(player:get_player_name(), minetest.registered_items)
 end)
 
-creative.register_tab("all", "All", minetest.registered_items)
-creative.register_tab("nodes", "Nodes", minetest.registered_nodes)
-creative.register_tab("tools", "Tools", minetest.registered_tools)
-creative.register_tab("craftitems", "Items", minetest.registered_craftitems)
+creative.register_tab("all", S("All"), minetest.registered_items)
+creative.register_tab("nodes", S("Nodes"), minetest.registered_nodes)
+creative.register_tab("tools", S("Tools"), minetest.registered_tools)
+creative.register_tab("craftitems", S("Items"), minetest.registered_craftitems)
 
 local old_homepage_name = sfinv.get_homepage_name
 function sfinv.get_homepage_name(player)
